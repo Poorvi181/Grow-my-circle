cd1626e5f0e81f4be1bcd27843361c5a034c9472
Poorvi181 <mannarchana@yahoo.co.uk>
2025-01-28 18:16:14 +0000

    Update Grow my circle.py

--- Grow my circle.py
+++ Grow my circle.py
@@ -12,11 +12,22 @@ class Circle:
         self.surface=screen
     def draw(self):
         pygame.draw.circle(self.surface,self.color,self.pos,self.radius,self.width)
+    def grow(self,r):
+        self.radius=self.radius+r
+        pygame.draw.circle(self.surface,self.color,self.pos,self.width)
+
 circle=Circle((300,300),10,0)
 circle.draw()
-pygame.display.update()
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             sys.exit()
+        if event.type==pygame.MOUSEBUTTONUP:
+            screen.fill("black")
+            circle.draw()
+            pygame.display.update()
+        elif event.type==pygame.MOUSEBUTTONDOWN:
+            screen.fill("black")
+            circle.grow(10)
+            pygame.display.update()
 
